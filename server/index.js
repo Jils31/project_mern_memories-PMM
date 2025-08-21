1b89c1784a4b60408e4f447cef9801922fd2b305
Jils31 <jilspatel21@gmail.com>
2025-08-21 23:27:38 +0530

    Added security measures

--- server/index.js
+++ server/index.js
@@ -2,8 +2,11 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
+
+dotenv.config();
 
 const app = express();
 
@@ -13,7 +16,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://jilspatel21:lrkIEJ4HglawisV4@cluster0.2folupb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const CONNECTION_URL = process.env.MONGODB_URI
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL)
